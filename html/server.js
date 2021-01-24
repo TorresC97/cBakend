5002f9d382aeefecd777e77789169a6291cf1d98
Christian <cristrs97@gmail.com>
2021-01-24 00:09:56 -0600

    the views engine was added whit ejs
    it becomes a dynamic file whit: re.render('index')

--- html/server.js
+++ html/server.js
@@ -4,17 +4,14 @@ const express = require('express');
 //ejecutamos la funcion parta obtener el objeto app por el cual definimos rutas
 const app = express();
 
-app.use('view engine','ejs')
+app.set('view engine','ejs')
 
 app.use('/static',express.static('static')); //middleware
 
 
 //Ruta inicial que responde a un docuemnto HTML
 app.get('/',function(req,res){
-    res.sendFile('index.html',{
-        root: __dirname
-    });
-    //res.send(__dirname); //------>  Nos da el PAHT absoluto sin nesecidad de escribirlo, se actualiza solo con el ___dirname
+    res.render('index');
 });
 
 //ponemos a escuchar a nuestro servidor
